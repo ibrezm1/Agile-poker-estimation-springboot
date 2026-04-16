c4cd58c66deaa7d442285f5226fe76c76afc43a8
ibrezm <ibrezmm@MacBookAir.lan>
2026-04-16 14:53:22 -0400

    test

--- src/main/java/com/example/voter/demo/DemoApplication.java
+++ src/main/java/com/example/voter/demo/DemoApplication.java
@@ -2,7 +2,6 @@ package com.example.voter.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
@@ -12,5 +11,4 @@ public class DemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
-
 }
